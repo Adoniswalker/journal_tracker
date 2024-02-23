78b9e4e2b7d83bf0406f5544a0bdc9919b4ed5ed
Dennis Ngeno <dngeno@songa.app>
2024-02-23 14:47:34 +0300

    Add typeOrm

---
+++ src/entities/User.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+    IsEmail,
+} from "class-validator";
+
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    @IsEmail()
+    email: string
+
+    @Column()
+    password: string
+}
